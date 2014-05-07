3ebcff095511330333e613f254d923fd8eb4d593
Seamus Abergel-Preston <technologygeek@rogers.com>
2014-05-07 09:52:39 -0400

    Added basic scripts

---
+++ Basic_calculator.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+print("By !ns!gn!a")
+print("Copyright BlackStallion Labs")
+print("Random Number thing")
+num1 = input("Insert Number 1: ")
+num2 = input("Insert number 2: ")
+print (num2 + num1)
---
+++ Monty_Python.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+#Script by Seamus Abergel-Preston
+#Copyright BlackStallion Labs
+#This line prints the author's info
+print("By !ns!gn!a")
+print("Copyright BlackStallion Labs")
+#This section sets the variables 'name, quest, and color'
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+#This section prints the feedback lines and decoration
+print("=========================================|=<>=<>=<>=<>=<>=|=========================================")
+print("So you mean to tell me that your name is %s, your quest is to %s, " \
+"and your favorite color is %s?" % (name, quest, color))
+print("=========================================|=<>=<>=<>=<>=<>=|=========================================")
