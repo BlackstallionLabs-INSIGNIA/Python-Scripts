f4b1d478f6bca3147cd91b0bf1317d0ef42a2891
Seamus Abergel-Preston <technologygeek@rogers.com>
2014-05-09 11:01:06 -0400

    Redid 90% of code.
    Now more efficient and with cool scroll effects.

--- menu.py
+++ menu.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
+
 import time
-print (25 * '-')
+print ('By !NS!GN!A & Blackstallion Labs.')
+print (29 * '-')
 time.sleep(.1)
-print ("   B O O T - M E N U")
+print ("   P R O G R A M - M E N U")
 time.sleep(.1)
-print (25 * '-')
+print (29 * '-')
 time.sleep(.1)
 print ("1. About")
 time.sleep(.1)
@@ -13,7 +15,7 @@ time.sleep(.1)
 print ("3. Addition Calculator")
 time.sleep(.1)
 
-print (25 * '-')
+print (29 * '-')
 
 ## Get input ###
 choice = input('Enter your choice [1-3] : ')
@@ -25,11 +27,13 @@ choice = int(choice)
 if choice == 1:
         print('Loading...')
         time.sleep(.1)
-        print(25 * '-')
-        print('This script was designed by !NS!GN!A for Blackstallion labs. ' 
+        print(29 * '-')
+        print('This menu script was designed by !NS!GN!A for Blackstallion labs. ' 
         'Each week, check my github repo for new scripts, whick will be added '
-        'to this menu. Just redownload it and you are good to go!')
-        print (25 * '-')
+        'to this menu. Just redownload it and you are good to go!' ' If you use ' 
+        'this script in any of your projects please DO NOT delete the `about` section.'
+        ' It is the only way that I get credit for this script.' )
+        print (29 * '-')
        
         
         
@@ -37,27 +41,27 @@ if choice == 1:
 elif choice == 2:
         print('Starting Game...')
         time.sleep(.1)
-        print(25 * '-')
+        print(29 * '-')
         name = input("What is your name?")
         quest = input("What is your quest?")
         color = input("What is your favorite color?")
         
-        
+        print (29 * '-')
         print("So you mean to tell me that your name is %s, your quest is to %s, " \
         "and your favorite color is %s?" % (name, quest, color))
-        print (25 * '-')
+        print (29 * '-')
         
 
 elif choice == 3:
         print ("Starting Calculator...")
         time.sleep(.1)
-        print(25 * '-')
+        print(29 * '-')
         num1 = input("Insert Number 1: ")
         num2 = input("Insert number 2: ")
-        print (num2 + num1)
-        print (25 * '-')
+        print (num2 + num1)('is the answer')
+        print (29 * '-')
         
 
 else:
         print ("Invalid number. Try again...")
-        print (25 * '-')
+        print (29 * '-')
